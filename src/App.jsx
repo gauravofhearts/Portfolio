f7981ad6eb47b2803cce5c23415eb4532a012840
gauravofhearts <gauravofhearts@gmail.com>
2024-10-04 02:25:02 +0530

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ import Skills from './pages/Skills';
 import Portfollio from './pages/Portfollio';
 import Contact from './pages/Contact';
 import Footer from './components/Footer/Footer';
-import ScrollToTop from './components/scrolltoTop';
+
 
 
 const App = () => {
   return (
     <>
     <Router>
-    <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
