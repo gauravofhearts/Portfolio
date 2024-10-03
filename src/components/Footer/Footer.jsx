d5248ff034dd715f87ee06414cac67571c6a4d30
gauravofhearts <gauravofhearts@gmail.com>
2024-10-04 01:51:14 +0530

    fix code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css'; // External CSS for styling
+import { Link } from 'react-router-dom'; 
 
 const Footer = () => {
   return (
@@ -16,19 +17,19 @@ const Footer = () => {
         <div className="footer-section links">
           <h3>Quick Links</h3>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Skills</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/skills">Skills</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
 
         <div className="footer-section social">
           <h3>Follow Us</h3>
           <div className="social-links">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="https://www.facebook.com/share/7ZcTCsaBZo8F8T5Z/?mibextid=qi2Omg" target="_blank" rel="noopener noreferrer">Facebook</a>
+            <a href="https://www.linkedin.com/in/gaurav-srivastav-13b818262?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">Linkedln</a>
+            <a href="https://www.instagram.com/gauravofhearts?igsh=d3NteDY5aWEwaGpr" target="_blank" rel="noopener noreferrer">Instagram</a>
           </div>
         </div>
       </div>
--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ContactForm.css';
 
+
 // Contact Form Component
 const ContactForm = () => {
   return (
@@ -30,7 +31,7 @@ const Map = () => {
   return (
     <iframe
       title="Our Location"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.162898625551!2d-122.41941618468132!3d37.7749292797591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808efcded2bf%3A0x88fdb04c6c66d36e!2sSan%20Francisco%2C%20CA%2C%20USA!5e0!3m2!1sen!2sin!4v1609800170243!5m2!1sen!2sin"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.1016307647374!2d77.37119297375229!3d28.62671598434794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce5447d350e9f%3A0x4beba507fa3f455b!2sThe%20Iconic%20Corenthum!5e0!3m2!1sen!2sin!4v1727986326628!5m2!1sen!2sin"
       width="100%"
       height="500"
       style={{ border: '0' }}
